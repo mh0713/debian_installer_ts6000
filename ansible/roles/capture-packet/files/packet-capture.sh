6ff53872fd20db8627a8c74a7ae726b1740c2ddd
Masumisu Hattamasumitsu.hatta@gmail.com <masumitsu.hatta@gmail.com>
2023-08-28 09:29:29 +0900

    fluent-bit / nginx / capture-packet まで完了

--- ansible/roles/capture-packet/files/packet-capture.sh
+++ ansible/roles/capture-packet/files/packet-capture.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source /etc/packet-capture.conf
+
 SECONDS=1800
 ROTATES=16
-PCAP_DIR="/mnt/array1/pcap"
+PCAP_DIR="${DATA_DIR}/pcap"
 CAPTURE_IF=enp8s0f1
 HOSTNAME=$(hostname -s)
 
@@ -16,6 +18,6 @@ do
   esac
 done
 
-/usr/sbin/tcpdump -i ${CAPTURE_IF} -nn -s 0 -G ${SECONDS} -W ${ROTATES} -w ${PCAP_DIR}/${HOSTNAME}-%Y%m%d-%H%M%S.pcap
+/usr/bin/tcpdump -i ${CAPTURE_IF} -nn -s 0 -G ${SECONDS} -W ${ROTATES} -w ${PCAP_DIR}/${HOSTNAME}-%Y%m%d-%H%M%S.pcap
 
 exit 0
